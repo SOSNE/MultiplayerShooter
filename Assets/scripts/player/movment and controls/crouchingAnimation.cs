e2f26264f68e26dbb923fe3604421d3b74563356
SOSNE <tomjansan@gmail.com>
2024-09-02 19:41:53 +0200

    Added crouching system.

---
+++ Assets/scripts/player/movment and controls/crouchingAnimation.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using Unity.Netcode;
+using UnityEngine;
+
+public class crouchingAnimation : NetworkBehaviour
+{
+    [SerializeField] private GameObject _weapon;
+    private bool _crouch;
+    void Start()
+    {
+        _crouch = false;
+    }
+    
+    void Update()
+    {
+        if (!IsOwner) return;
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            _crouch = true;
+            // StartCoroutine(DrawLine(_weapon, 0.3f));
+            SetWalkServerRpc(_crouch, gameObject);
+            
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            _crouch = false;
+            SetWalkServerRpc(_crouch, gameObject);
+        }
+        
+    }
+    [ServerRpc]
+    void SetWalkServerRpc(bool value, NetworkObjectReference playerNetworkObjectReference) {
+        CrouchClientRpc(value, playerNetworkObjectReference);
+    }
+
+    [ClientRpc]
+    void CrouchClientRpc(bool value, NetworkObjectReference playerNetworkObjectReference) {
+        if(playerNetworkObjectReference.TryGet(out NetworkObject playerNetworkObject))
+        {
+            playerNetworkObject.GetComponent<Animator>().SetBool("crouching", value);
+            if (value)
+            {
+                playerNetworkObject.GetComponent<CapsuleCollider2D>().offset = new Vector2(0.02604413f, 0.1193484f);
+                playerNetworkObject.GetComponent<CapsuleCollider2D>().size = new Vector2(0.5381981f, 2.677239f);
+            }
+            else
+            {
+                playerNetworkObject.GetComponent<CapsuleCollider2D>().offset = new Vector2(0.02604413f, 0.4789118f);
+                playerNetworkObject.GetComponent<CapsuleCollider2D>().size = new Vector2(0.5381981f, 3.396366f);
+            }
+        }
+    }
+
+    // IEnumerator DrawLine(GameObject weapon, float duration)
+    // {
+    //
+    //     Vector2 weaponStartPosition = weapon.transform.position;
+    //     float startTime = Time.time;
+    //     while (Time.time - startTime < duration && weaponStartPosition.y != weaponStartPosition.y - 1)
+    //     {
+    //         float t = (Time.time - startTime) / duration;
+    //
+    //         Vector2 currentPosition = Vector2.Lerp(weaponStartPosition,
+    //             new Vector2(weaponStartPosition.x, weaponStartPosition.y - 1), t);
+    //         weapon.transform.position = currentPosition;
+    //         yield return null;
+    //     }
+    // }
+}
