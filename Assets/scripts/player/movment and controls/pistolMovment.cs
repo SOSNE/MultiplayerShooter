74861003d15207c9a1292f6bf108b0104f5465ef
SOSNE <tomjansan@gmail.com>
2024-09-14 16:26:42 +0200

    Fix to Bézier curve positions. Added rotation recoil calculation and implementation.

--- Assets/scripts/player/movment and controls/pistolMovment.cs
+++ Assets/scripts/player/movment and controls/pistolMovment.cs
@@ -9,7 +9,7 @@ public class pistolMovment : NetworkBehaviour
     public Transform targetL, targetR, positionL, positionR, positionFirstL, positionFirstR;
     public double maxExtension = 1.11931;
     public Camera camera;
-    private float width;
+    private float width, _rotationRecoilAngle, _angle;
     private Vector3 _currentWeaponRecoilPosition;
     
     
@@ -46,15 +46,14 @@ public class pistolMovment : NetworkBehaviour
         angleCorrection = (180 / Mathf.PI) * angleCorrection - 180;
         
         
-        float angle = Mathf.Atan2(mouseWorldPosition.y - transform.position.y, mouseWorldPosition.x - transform.position.x);
-        angle = (180 / Mathf.PI) * angle;
+        _angle = Mathf.Atan2(mouseWorldPosition.y - transform.position.y, mouseWorldPosition.x - transform.position.x);
+        _angle = (180 / Mathf.PI) * _angle;
         
         
         
         if (leftArmExtenstion <= maxExtension || rightArmExtenstion <= maxExtension)
         {
-                
-                // mouseWorldPosition = transform.parent.InverseTransformPoint(mouseWorldPosition) + _currentWeaponRecoilPosition;
+                // local mouse position in relation to player
                 Vector3 weaponTargetPosition = transform.parent.InverseTransformPoint(new Vector3(mouseWorldPosition.x, mouseWorldPosition.y));
                 
                 Vector3 counterRecoilPosition = weaponTargetPosition + _currentWeaponRecoilPosition * 0.2f;
@@ -63,10 +62,12 @@ public class pistolMovment : NetworkBehaviour
         }
         else 
         {
+            
             if (Vector2.Distance(mouseWorldPosition, transform.position) > 0.3f)
             {
-                transform.rotation = Quaternion.Euler(new Vector3(0f,0f, angle + Convert.ToSingle(angleCorrection)));
+                transform.rotation = Quaternion.Euler(new Vector3(0f,0f, _angle + Convert.ToSingle(angleCorrection) + _rotationRecoilAngle));
             }
+            
             if (_currentWeaponRecoilPosition != new Vector3(0, 0, 0))
             {
                 transform.localPosition = _currentWeaponRecoilPosition;
@@ -80,15 +81,15 @@ public class pistolMovment : NetworkBehaviour
         }
     }
     
-    IEnumerator WeaponRecoil(Transform weapon,float duration)
+    IEnumerator WeaponRecoil(Transform weapon,float duration,float recoilAngleIncrease = 40, float recoilScale = 0.4f)
     {
         List<Vector3> recoilBezierCurvesList = new List<Vector3>();
         
         float distance = 0.3f;
-        Vector3 bottomLeft = transform.localPosition + new Vector3(-distance, -distance, 0); // Bottom-left corner
-        Vector3 topLeft = transform.localPosition + new Vector3(-distance, distance, 0);     // Top-left corner
-        Vector3 topRight = transform.localPosition + new Vector3(distance, distance, 0);     // Top-right corner
-        Vector3 bottomRight = transform.localPosition + new Vector3(distance, -distance, 0); // Bottom-right corner
+        Vector3 bottomLeft = transform.localPosition; // Bottom-left corner
+        Vector3 topLeft = transform.localPosition + new Vector3(0.38f, 0.34f, 0) * recoilScale;     // Top-left corner
+        Vector3 topRight = transform.localPosition + new Vector3(1.3f, 0.56f, 0) * recoilScale;     // Top-right corner
+        Vector3 bottomRight = transform.localPosition + new Vector3(1.5f, 0.57f, 0) * recoilScale; // Bottom-right corner
 
         recoilBezierCurvesList.Add(bottomLeft);
         recoilBezierCurvesList.Add(topLeft);
@@ -104,8 +105,15 @@ public class pistolMovment : NetworkBehaviour
                                            3 * Mathf.Pow((1 - t), 2) * t * recoilBezierCurvesList[1] +
                                            3 * (1 - t) * Mathf.Pow(t, 2) * recoilBezierCurvesList[2] +
                                            Mathf.Pow(t, 3) * recoilBezierCurvesList[3];
+            _rotationRecoilAngle = -Mathf.Lerp(0, recoilAngleIncrease, t);
+            
+            
+            print(_rotationRecoilAngle);
             yield return null;
         }
+        // return weapon position to its starting position
+        transform.localPosition = bottomLeft;
         _currentWeaponRecoilPosition = Vector3.zero;
+        _rotationRecoilAngle = 0;
     }
 }
