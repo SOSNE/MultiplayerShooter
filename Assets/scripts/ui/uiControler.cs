a0a0925d3357245f0ec1fef32a92097de09f5b5d
SOSNE <tomjansan@gmail.com>
2024-09-19 16:07:55 +0200

    Code clean up.

--- Assets/scripts/ui/uiControler.cs
+++ Assets/scripts/ui/uiControler.cs
@@ -6,19 +6,11 @@ public class uiControler : NetworkBehaviour
 {
     public TextMeshProUGUI ammoCounter;
     [SerializeField] private TextMeshProUGUI hpCounter;
-    void Start()
-    {
-        
-    }
     
     void Update()
     {
         float remainingBullets = weaponHandling.BulletCount - weaponHandling.BulletCounter;
         ammoCounter.text = $"Bulets: {weaponHandling.BulletCount} / {remainingBullets}";
-        // if (PlayerHhandling.clientHealthMap.ContainsKey(NetworkManager.Singleton.LocalClientId))
-        // {
-        //     hpCounter.text = "Hp: "+ PlayerHhandling.clientHealthMap[NetworkManager.Singleton.LocalClientId];
-        // }
     }
     
     [ClientRpc]
@@ -26,11 +18,4 @@ public class uiControler : NetworkBehaviour
     {
         hpCounter.text = "Hp: "+ currentHealth;
     }
-
-    // [ServerRpc]
-    // private void GetHealthForUiServerRpc(ulong localClientId)
-    // {
-    //     GetHealthForUiClientRpc(PlayerHhandling.clientHealthMap[localClientId]);
-    // }
-    
 }
