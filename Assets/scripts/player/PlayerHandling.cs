6b280c13d8238dfad4a5cf39ae910e6a08322987
SOSNE <tomjansan@gmail.com>
2024-09-26 20:39:55 +0200

    Added basic ragdoll (currently not working, problem with rigidbody component).

--- Assets/scripts/player/PlayerHandling.cs
+++ Assets/scripts/player/PlayerHandling.cs
@@ -2,13 +2,19 @@ using System;
 using System.Collections.Generic;
 using TMPro;
 using Unity.Netcode;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.U2D.IK;
+
 
 public class PlayerHhandling : NetworkBehaviour
 {
     // NetworkBehaviourpublic NetworkList<int> playersHealth = new NetworkList<int>();
     public static Dictionary<ulong, int> clientHealthMap = new Dictionary<ulong, int>();
     private GameObject _gameManager;
+
+    [SerializeField]
+    private List<HingeJoint2D>  playerHingeJoints2d;
     
     
     public override void OnNetworkSpawn()
@@ -21,10 +27,13 @@ public class PlayerHhandling : NetworkBehaviour
         }
     }
 
-    // private void Update()
-    // {
-    //     uiControler.DisplayHp(clientHealthMap[NetworkManager.Singleton.LocalClientId]);
-    // }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Y))
+        {
+            PerformRagdollOnPlayer(transform);
+        }
+    }
 
     private void OnClientConnected(ulong clientId)
     {
@@ -67,6 +76,7 @@ public class PlayerHhandling : NetworkBehaviour
         if (clientHealthMap[clientId]<=0)
         {
             gameObject.GetComponent<GameManager>().HandleGame(currentClientId, clientId);
+            PerformRagdollOnPlayer(transform);
         }
         
         // update health ui
@@ -79,5 +89,36 @@ public class PlayerHhandling : NetworkBehaviour
         };
         GameObject.Find("UiControler").GetComponent<uiControler>()
             .GetHealthForUiClientRpc(clientHealthMap[clientId], clientRpcParams);
+    }
+
+    public void PerformRagdollOnPlayer(Transform playerTarget)
+    {
+        playerTarget.GetComponent<IKManager2D>().enabled = false;
+        playerTarget.GetComponent<Animator>().enabled = false;
+        playerTarget.GetComponent<playerMovment>().enabled = false;
+        // playerTarget.GetComponent<Rigidbody2D>().simulated = false;
+        playerTarget.GetComponent<Rigidbody2D>().simulated = false;
+        playerTarget.GetComponent<CapsuleCollider2D>().enabled = false;
+        playerTarget.GetComponent<wlakingAnimation>().enabled = false;
+        playerTarget.GetComponent<crouchingAnimation>().enabled = false;
+        SetLayerRecursively(transform.Find("bodyDown").gameObject, 0);
+        transform.Find("bodyDown").AddComponent<Rigidbody2D>();
+
+        foreach (var joint2D in playerHingeJoints2d)
+        {
+            joint2D.enabled = true;
+            joint2D.gameObject.AddComponent<Rigidbody2D>();
+
+        }
+    }
+    
+    void SetLayerRecursively(GameObject obj, int layer)
+    {
+        obj.layer = layer;
+
+        foreach (Transform child in obj.transform)
+        {
+            SetLayerRecursively(child.gameObject, layer);
+        }
     }
 }
