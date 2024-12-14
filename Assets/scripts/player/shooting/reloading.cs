7cdd188c153b2da7069bb44d1ffb83fe2154ebe2
SOSNE <tomjansan@gmail.com>
2024-12-14 14:34:32 +0100

    Added reloading animation.

--- Assets/scripts/player/shooting/reloading.cs
+++ Assets/scripts/player/shooting/reloading.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 using Unity.Netcode;
 using Unity.VisualScripting;
 
+
+// Total hours wasted: 5
+// Status: still not working xd
+// Status: something started working juppi
+
 public class reloading : NetworkBehaviour
 {
     public Transform leftHandTarget, magazineSpawningTarget;
     private Transform _magazine;
     public Transform[] waypoint;
-    private int _index = 0;
-    private bool _isCoroutineRunning = false;
+    public bool _isCoroutineRunning = false;
     public GameObject magazine;
     
     
@@ -23,85 +27,117 @@ public class reloading : NetworkBehaviour
     private bool _ejectMagazine = false;
     void Update()
     {
-        if(!IsOwner) return;
+        // if(!IsOwner) return;
         // if (_createdMagazine != null)
         // {
         //     _magazine = _createdMagazine.transform;
         // }
-        if (Input.GetKey(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.R) && !_isCoroutineRunning)
         {
-            // _ejectMagazine = true;
             weaponHandling.BulletCounter = 0;
+            _isCoroutineRunning = true;
+            PerformReloadingServerRpc(transform.parent.gameObject);
         }
-    //     if (IsInside(transform, _magazine) && _ejectMagazine)
-    //     {
-    //         Vector2 backDirection = -transform.up;
-    //         _magazine.Translate(backDirection * 1f * Time.deltaTime, Space.World);
-    //         
-    //     }else if(!IsInside(transform, _magazine) && _ejectMagazine)
-    //     {
-    //         _ejectMagazine = false;
-    //         _magazine.transform.SetParent(null);
-    //         _magazine.GetComponent<Rigidbody2D>().simulated = true;
-    //         _magazine.GetComponent<Rigidbody2D>().gravityScale = 2;
-    //         if (!_isCoroutineRunning)
-    //         {
-    //             StartCoroutine(GrabMagazine());
-    //         }
-    //     }
-    //     if (_createdMagazine != null && !_ejectMagazine && !_isCoroutineRunning)
-    //     {
-    //         _createdMagazine.transform.position = magazineSpawningTarget.position;
-    //         _createdMagazine.transform.rotation = magazineSpawningTarget.rotation;
-    //     }
-    // }
-    //
-    // bool IsInside(Transform outer, Transform inner)
-    // {
-    //     Bounds outerBounds = outer.GetComponent<Renderer>().bounds;
-    //     Bounds innerBounds = inner.GetComponent<Renderer>().bounds;
-    //
-    //     return innerBounds.Intersects(outerBounds);
-    // }
-    //
-    //
-    // IEnumerator GrabMagazine()
-    // {
-    //     _isCoroutineRunning = true;
-    //     leftHandTarget.SetParent(waypoint[2]);
-    //     while (_index < waypoint.Length)
-    //     {
-    //         while (Vector2.Distance(leftHandTarget.position, waypoint[_index].position) > 0.01f)
-    //         {
-    //             leftHandTarget.position = Vector2.MoveTowards(leftHandTarget.position, waypoint[_index].position, 7 * Time.deltaTime);
-    //             if (_createdMagazine != null && _index > 2)
-    //             {
-    //                 _createdMagazine.transform.position = leftHandTarget.position;
-    //             }
-    //             yield return null; 
-    //         }
-    //         
-    //         if (_index == 2)
-    //         {
-    //             yield return new WaitForSeconds(2f);
-    //             _createdMagazine = Instantiate(magazine, leftHandTarget.position, magazine.transform.rotation);
-    //             _createdMagazine.GetComponent<NetworkObject>().Spawn(true);
-    //         }
-    //
-    //         if (_index == waypoint.Length - 1)
-    //         {
-    //             if (_createdMagazine != null)
-    //             {
-    //                 Destroy(_createdMagazine);
-    //                 _createdMagazine = Instantiate(magazine, magazineSpawningTarget.position, transform.rotation);
-    //                 leftHandTarget.SetParent(transform);
-    //                 _createdMagazine.GetComponent<NetworkObject>().Spawn(true);
-    //             }
-    //         }
-    //         _index++;
-    //     }
-    //
-    //     _index = 0;
-    //     _isCoroutineRunning = false;   
+        
+        // else if(!IsInside(transform, _magazine) && _ejectMagazine)
+        // {
+        //     _ejectMagazine = false;
+        //     _magazine.transform.SetParent(null);
+        //     _magazine.GetComponent<Rigidbody2D>().simulated = true;
+        //     _magazine.GetComponent<Rigidbody2D>().gravityScale = 2;
+        //     if (!_isCoroutineRunning)
+        //     {
+        //         StartCoroutine(GrabMagazine());
+        //     }
+        // }
+        // if (_createdMagazine != null && !_ejectMagazine && !_isCoroutineRunning)
+        // {
+        //     _createdMagazine.transform.position = magazineSpawningTarget.position;
+        //     _createdMagazine.transform.rotation = magazineSpawningTarget.rotation;
+        // }
+    }
+    
+    bool IsInside(Transform outer, Transform inner)
+    {
+        Bounds outerBounds = outer.GetComponent<Renderer>().bounds;
+        Bounds innerBounds = inner.GetComponent<Renderer>().bounds;
+    
+        return innerBounds.Intersects(outerBounds);
+    }
+
+    [ServerRpc]
+    private void PerformReloadingServerRpc(NetworkObjectReference playerGameObject)
+    {
+        PerformReloadingClientRpc(playerGameObject);
+    }
+
+    [ClientRpc]
+    private void PerformReloadingClientRpc(NetworkObjectReference playerGameObject)
+    {
+        if (playerGameObject.TryGet(out NetworkObject playerNetworkObject))
+        {
+            playerNetworkObject.transform.Find("pistol_0").
+                GetComponent<reloading>().StartCoroutine(GrabMagazine());
+        }
+    }
+    
+    IEnumerator GrabMagazine()
+    {
+        
+        _magazine = Instantiate(magazine, magazineSpawningTarget.position, transform.rotation).transform;
+        _magazine.SetParent(transform);
+        Vector2 backDirection;
+        if (transform.localScale == new Vector3(-1, -1, 1))
+        {
+            backDirection = transform.up;
+        }
+        else
+        {
+            backDirection = -transform.up;
+        }
+        
+        while (IsInside(transform, _magazine))
+        {
+            _magazine.Translate(backDirection * 1f * Time.deltaTime, Space.World);
+            
+            yield return null;
+        }
+        
+        _magazine.transform.SetParent(null);
+        _magazine.GetComponent<Rigidbody2D>().simulated = true;
+        _magazine.GetComponent<Rigidbody2D>().gravityScale = 2;
+
+
+        leftHandTarget.SetParent(waypoint[2]);
+        int index = 0;
+        while (index < waypoint.Length)
+        {
+            while (Vector2.Distance(leftHandTarget.position, waypoint[index].position) > 0.01f)
+            {
+                leftHandTarget.position = Vector2.MoveTowards(leftHandTarget.position, waypoint[index].position,
+                    2 * Time.deltaTime);
+                // if (_createdMagazine != null && _index > 2)
+                // {
+                //     // _createdMagazine.transform.position = leftHandTarget.position;
+                // }
+                yield return null;
+            }
+            
+            if (index == 2)
+            {
+                yield return new WaitForSeconds(0.3f);
+                _createdMagazine = Instantiate(magazine, leftHandTarget.position, magazine.transform.rotation);
+                _createdMagazine.transform.SetParent(leftHandTarget);
+            }
+
+            if (index == waypoint.Length - 1)
+            {
+                leftHandTarget.SetParent(transform);
+                Destroy(_createdMagazine);
+            }
+            index++;
+        }
+
+        _isCoroutineRunning = false;
     }
 }
